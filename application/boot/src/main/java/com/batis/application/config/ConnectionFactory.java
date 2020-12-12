1b0e5749550d96452323ec9bd7a675c08233d8af
Weixin <yanweixin@zoho.com>
2020-12-12 10:19:32 +0800

    Update gradle and fix build bugs

--- application/boot/src/main/java/com/batis/application/config/ConnectionFactory.java
+++ application/boot/src/main/java/com/batis/application/config/ConnectionFactory.java
@@ -1,33 +1,50 @@
 package com.batis.application.config;
 
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
     private interface Singleton {
         ConnectionFactory INSTANCE = new ConnectionFactory();
     }
 
+    private final Properties properties;
     private final DataSource dataSource;
 
+    private ConnectionFactory() {
+        this.properties = readProperties();
+        this.dataSource = getDataSource();
+    }
+
+    private synchronized Properties readProperties() {
+        if (properties == null) {
+            YamlPropertiesFactoryBean factoryBean = new YamlPropertiesFactoryBean();
+            factoryBean.setResources(new ClassPathResource("application.yml"));
+            return factoryBean.getObject();
+        }
+        return properties;
+    }
+
     private DataSource getDataSource() {
+        String url = System.getenv().getOrDefault("SPRING_DATASOURCE_URL", properties.getProperty("spring.datasource.url"));
+        String driverClassName = properties.getProperty("spring.datasource.driver-class-name");
         return DataSourceBuilder
                 .create()
-                .driverClassName("com.mysql.cj.jdbc.Driver")
-                .url("jdbc:mysql://dev-server:3306/log")
+                .driverClassName(driverClassName)
+                .url(url.substring(0, url.lastIndexOf('/')) + "/log")
                 .username("logs")
                 .password("logs")
                 .build();
-    }
-
-    private ConnectionFactory() {
-        this.dataSource = getDataSource();
     }
 
     public static Connection getDatabaseConnection() throws SQLException {
         return Singleton.INSTANCE.dataSource.getConnection();
     }
 }
+
